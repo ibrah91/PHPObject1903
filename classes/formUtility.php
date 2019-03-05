9000ee74c2266bcf2878c7f8e2859afdbff9b597
ibrah91 <ibrahima.traore@live.fr>
2019-03-05 12:41:47 +0100

    version5

--- classes/formUtility.php
+++ classes/formUtility.php
@@ -5,15 +5,59 @@
  * ********* */
 class FormUtility
 {
-public function generateHTML(): string {
+public function generateHTML(
+        string $label,
+        string $type,
+        string $id,
+        ?string $placeholder=''
+):string{
+        //l'eventuel message d'erreur
+        $oldValue = (isset($_POST[$id])) ? $_POST[$id] : '';
+        //On vérifie le type pour fournir soit :
+        //Un input : text , number
+        //Un text area :textarea
+        switch($type){
+        case 'text':
+        case 'number':
+               $widget = $this->getHTMLInput($type,$id,$oldValue,$placeholder);
+         break;
+         case 'textarea':
+               $widget = $this->getHTMLTextArea($id,$oldValue,$placeholder);
+        break;
+                        default;
+                        die('Le type donné est inconnu');
+        }
+
     return <<<EOT
     <div class="form-group">
-    <label for ="item">Label</label>
-    <input type="text" id="item"
- class="form-control"/>
+    <label for="$id">$label</label>
+    $widget
  </div>
 EOT;
         }
+
+
+
+private function getHTMLInput(string $type, string $id , string $oldValue , ?string $placeholder){
+        return <<<EOT
+<input type="$type" id="$id" name="$id" class="form-control" 
+placeholder="$placeholder" value="$oldValue"/>
+ </div>
+EOT;
+
+}
+
+private function getHTMLTextArea(string $id , string $oldValue , ?string $placeholder=''){
+        return <<<EOT
+<textarea id="$id" name="$id" class="form-control" 
+placeholder="$placeholder"/>
+$oldValue
+ </div>
+EOT;
+
+
+}
+
 }
 
 
--- formulaire-produit-auto.php
+++ formulaire-produit-auto.php
@@ -8,9 +8,9 @@ $formUtility = new FormUtility();
 <main class="container">
     <h1>Création d'un nouveau produit</h1>
     <form method="post">
-    <?=$formUtility->generateHTML('Nom du produit');?>
-    <?=$formUtility->generateHTML('Price du produit');?>
-    <?=$formUtility->generateHTML('Description');?>
+    <?= $formUtility->generateHTML('Nom du produit','text','name','saisir le nom du produit'); ?>
+    <?= $formUtility->generateHTML('Price du produit','number','price','saisir le prix'); ?>
+    <?= $formUtility->generateHTML('Description','textarea','description','description du produit'); ?>
 
     <button type="submit" class="btn btn-primary">Créer le produit</button>
     </form>
